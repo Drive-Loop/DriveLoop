e7f62ae88020809d0229a768961dc25726863d93
driveloop <https://github.com/Drive-Loop/>
2026-06-30 04:12:25 +0800

    fix: point mini train DD2 inference config at train data

--- dreamer-train/projects/DriveDreamer2/configs/drivedreamer2_img_cond_mini_train_local.py
+++ dreamer-train/projects/DriveDreamer2/configs/drivedreamer2_img_cond_mini_train_local.py
@@ -2,7 +2,11 @@ from .drivedreamer2_img_cond_mini_local import *
 
 train_data = '/data/projects/DriveLoop/data/processed/nuscenes/v1.0-mini/cam_all_train/v0.0.2'
 test_data = train_data
+project_dir = '/data/projects/DriveLoop/exp/drivedreamer2_img_cond_mini_train_local'
+save_path = '/data/projects/DriveLoop/outputs/drivedreamer2_img_cond_mini_train'
 
-dataloaders['train']['data_or_config'] = train_data
-dataloaders['test']['data_or_config'] = test_data
-dataloaders['test']['shuffle'] = False
+config['project_dir'] = project_dir
+config['dataloaders']['train']['data_or_config'] = train_data
+config['dataloaders']['test']['data_or_config'] = test_data
+config['dataloaders']['test']['shuffle'] = False
+config['test']['save_dir'] = save_path
